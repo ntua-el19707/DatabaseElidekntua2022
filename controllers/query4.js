a35420be9b86ce42c411ac7fe33469b4e14e6091
vgiann06 <106684093+vgiann06@users.noreply.github.com>
2022-06-06 00:00:07 +0300

    Add files via upload

--- controllers/query4.js
+++ controllers/query4.js
@@ -1,4 +1,3 @@
-exports.getMain = (req,res,next) => {
-           res.render('query4.ejs', {
-                pageTitle: "Organization with the same number of works 2 Years" })
+exports.getMain = (req,res,next) => {    
+ 
 }
